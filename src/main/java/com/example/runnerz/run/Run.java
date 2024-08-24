573d7c7f0461855c68467c8abce39476d7a8cd25
aejmcclelland <aejmcclelland@googlemail.com>
2024-08-24 10:34:48 +0100

    Added h2 dependency and sql

--- src/main/java/com/example/runnerz/run/Run.java
+++ src/main/java/com/example/runnerz/run/Run.java
@@ -1,14 +1,26 @@
 package com.example.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(
         Integer id,
+        @NotEmpty
         String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive
         Integer miles,
         Location location
 ) {
+
+    public Run{
+        if(startedOn.isAfter(completedOn)){
+            throw new IllegalArgumentException("Run cannot be completed before it starts");
+        }
+    }
 
 }
