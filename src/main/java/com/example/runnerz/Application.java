663ae2ce9723ed1dcd838a5eaf5100c1d008c734
aejmcclelland <aejmcclelland@googlemail.com>
2024-08-25 21:58:30 +0100

    Added User RESTClient functionality

--- src/main/java/com/example/runnerz/Application.java
+++ src/main/java/com/example/runnerz/Application.java
@@ -1,9 +1,16 @@
 package com.example.runnerz;
 
+
+import com.example.runnerz.user.User;
+import com.example.runnerz.user.UserRestClient;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -12,6 +19,19 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(UserRestClient client) {
+		return args -> {
+
+//			List<User> users = client.findAll();
+//			System.out.println(users);
+
+			User user = client.findById(2);
+			System.out.println(user);
+
+		};
 	}
 
 
